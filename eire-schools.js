8556e30843ff6cb82fb735a888140cf01d15c8ee
Daniel Rodas Bautista <danielrb@pop-os.localdomain>
2021-05-12 08:22:07 -0400

    small fixes

--- eire-schools.js
+++ eire-schools.js
@@ -28,33 +28,36 @@ const getDistance = async (destination) => {
     if (row.elements.length > 0 && row.elements[0].distance) {
       distance = row.elements[0].distance.text;
       duration = row.elements[0].duration.text;
-    // console.log({distance: row.elements[0].distance, duration: row.elements[0].duration})
+    console.log({distance: row.elements[0].distance, duration: row.elements[0].duration})
+    } else {
+      console.log(response);
     }
   })
   return {distance, duration};
 }
 
-// schools = schools.slice(1,4); // TODO: remove
+schools = schools.slice(1,4); // TODO: remove when not testing
 
 let distances = []
 
 const main = async () => {
   for (school of schools) {school: school['Official Name']
     const destination = `${school.Eircode}+Ireland`; // school['County Description'];
+    console.log(`Getting distance to ${destination} from ${origin}`)
     const {distance, duration} = await getDistance(destination);
-    // distances.push({school: school['Official Name'], distance, duration})
     distances.push({...school, distance, duration})
   }
 
-  // distances = distances.sort((d0, d1) => d0.duration > d1.duration)
+  // distances = distances.sort((d0, d1) => d0.duration > d1.duration) // TODO: figure out sorting
 
-  // console.log(distances)
-  // const worksheet = XLSX.utils.json_to_sheet(distances, {header: ['School', 'Distance']})
+
+  console.log('Writing output')
   const worksheet = XLSX.utils.json_to_sheet(distances)
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet)
   XLSX.writeFile(workbook, 'distances.xlsx')
-  console.log(XLSX.utils.sheet_to_csv(worksheet));
+
+  // console.log(XLSX.utils.sheet_to_csv(worksheet));
 }
 
 main();
